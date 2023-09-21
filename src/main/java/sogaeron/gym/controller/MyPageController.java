04113a6aae27d031ea9be3f22aaf7e3f257edf92
Leejh0426 <leejunho2000@gmail.com>
2023-09-21 18:45:20 +0900

    아마존서버연결 테스트용 코드 5(자동화시도중)

--- src/main/java/sogaeron/gym/controller/MyPageController.java
+++ src/main/java/sogaeron/gym/controller/MyPageController.java
@@ -1,7 +1,13 @@
 package sogaeron.gym.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyPageController {
+
+    @GetMapping("/mypage")
+    public String mypage(){
+        return "mypage";
+    }
 }
