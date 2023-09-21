57eb762a720113b6e7ecd469640f9a146bca0dce
Leejh0426 <leejunho2000@gmail.com>
2023-09-21 18:56:37 +0900

    아마존서버연결 테스트용 코드 6(자동화시도중)

--- src/main/java/sogaeron/gym/controller/ReservationController.java
+++ src/main/java/sogaeron/gym/controller/ReservationController.java
@@ -1,7 +1,14 @@
 package sogaeron.gym.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReservationController {
+
+    @GetMapping("/reservation")
+    public String reservation(){
+        return "reservation";
+    }
+
 }
