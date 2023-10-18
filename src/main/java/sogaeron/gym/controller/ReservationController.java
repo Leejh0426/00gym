2ed76806c9c73dc4798e288e8647cc9a2cb97b2e
Leejh0426 <leejunho2000@gmail.com>
2023-10-18 13:59:53 +0900

    임시 reservation값 수정 html

--- src/main/java/sogaeron/gym/controller/ReservationController.java
+++ src/main/java/sogaeron/gym/controller/ReservationController.java
@@ -1,14 +1,31 @@
 package sogaeron.gym.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sogaeron.gym.model.Reservation;
+import sogaeron.gym.service.ReservationService;
 
 @RestController
 public class ReservationController {
 
+    final
+    ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
     @GetMapping("/reservation")
     public String reservation(){
         return "reservation";
+    }
+
+    @PostMapping("/testreservation")
+    public void testreservation(@ModelAttribute Reservation reservation){
+        reservationService.testreservation(reservation);
     }
 
 }
--- src/main/java/sogaeron/gym/service/ReservationService.java
+++ src/main/java/sogaeron/gym/service/ReservationService.java
@@ -41,4 +41,7 @@ public class ReservationService {
 
     }
 
+    public void testreservation(Reservation reservation) {
+        reservationRepository.save(reservation);
+    }
 }
