ee067fc08e1f18e8942718051811786c6c7dbe12
Leejh0426 <leejunho2000@gmail.com>
2023-11-28 16:41:57 +0900

    컴네테스트용코드

--- src/main/java/sogaeron/gym/controller/ArduinoController.java
+++ src/main/java/sogaeron/gym/controller/ArduinoController.java
@@ -1,15 +1,23 @@
 package sogaeron.gym.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import sogaeron.gym.apiPayload.ApiResponse;
 import sogaeron.gym.controller.DTO.ArduinoCondDTO;
 import sogaeron.gym.model.GymStatus;
 import sogaeron.gym.service.ArduinoService;
 import sogaeron.gym.service.GymService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpHeaders;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+@Slf4j
 @RestController
 /**
  * 브라우저 상의 웹클라이언트의 아두이노 정보 관련 요청 및 응답을 처리하는 클래스
@@ -54,6 +62,37 @@ public class ArduinoController {
             return ApiResponse.onSuccess(arduinoCondDTO) ;
         }
 
+    }
+
+    /**
+     * 컴퓨터 네트워크 수업용 API (임시 ) - 준호,태규
+     */
+
+    @GetMapping("/test")
+    public String test(
+                       HttpMethod httpMethod,
+                       Locale locale,
+                       @RequestHeader MultiValueMap<String, String> headerMap,
+                       @RequestHeader("host") String host,
+                       @CookieValue(value = "myCookie", required = false) String cookie) {
+
+
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+
+        return "성공";
+
+    }
+
+    /**
+     * 컴퓨터 네트워크 수업용 API (임시 ) - 경원,정원
+     */
+    @GetMapping("/test1")
+    public String test() {
+        return "Hello, world!";
     }
 
 }
