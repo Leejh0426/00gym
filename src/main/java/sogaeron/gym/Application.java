b3e7042ad19819e8861dacf12717bb0bac342ef0
Leejh0426 <leejunho2000@gmail.com>
2023-12-01 14:17:58 +0900

    reservation조회 local에서는 정상적으로 작동하는데 서버에 올리면 시간대가 밀려서 작동해서 문제확인중2

--- src/main/java/sogaeron/gym/Application.java
+++ src/main/java/sogaeron/gym/Application.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class Application {
+
+	@PostConstruct
+	public void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
